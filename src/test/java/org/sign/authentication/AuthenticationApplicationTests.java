53c9de9047733e510d15e2724e0eb50b17dcc4b6
saikumar <ext.saikumar.veeranki@cevalogistics.com>
2022-12-29 10:30:43 +0530

    initial

---
+++ src/test/java/org/sign/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sign.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
